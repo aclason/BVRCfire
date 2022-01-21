26f9ed29c8e9270e121dc661d2bb0ec341d14473
aclason <ajclason@gmail.com>
2022-01-21 14:50:26 -0800

    BCWS weather and DOB prep edit

--- R/DataPrep/DOB_DataPrep.R
+++ R/DataPrep/DOB_DataPrep.R
@@ -15,7 +15,6 @@ if(length(new.packages)) install.packages(new.packages)
 lapply(ls, library, character.only = TRUE)  # load the required packages
 rm(ls, new.packages)
 
-
 #---------------- Load data --------------------#
 SpatialFilesPath <- "E:/Ingrid/Borealis/BVRCfire"
 
@@ -25,8 +24,7 @@ DOB_list <- list.files(paste0(SpatialFilesPath,"./Inputs/DOB/"),
                        recursive = FALSE, 
                        full.names=TRUE)
 print(paste("there are", length(DOB_list), "covariates in the list"))
-DOB_list <- DOB_list[!grepl("xml", DOB_list)]
-
+DOB_list <- DOB_list[!grepl("xml", DOB_list)] #AC: I don't think you need this line as you don't have any .xml files??
 
 #-----------------Round DOB pixels-----------------#
 DOB <- list()
@@ -36,13 +34,11 @@ file.name <- c("C10784", "C10970", "C11837", "C11937", "C12594", "C20729", "C207
                "C50744","G41607", "G51632", "K20637", "R11498", "R11796", "R11921", "R12068", 
                "R12315", "R21721", "VA1787", "VA1964")
 
-
 for (i in 1:length(DOB_list)){
-  DOB[[i]] <- raster(DOB_list[[i]])
+  DOB <- raster(DOB_list[[i]])
   # Round DOB values to whole numbers
-  DOB_rounded[[i]] <- round(DOB[[i]], digits = 0)
+  DOB_rounded <- round(DOB, digits = 0)
   # Write rasters
-  writeRaster(DOB_rounded[[i]], paste0(SpatialFilesPath, "./Inputs/DOB/DOBrounded/", "DOBrounded_", file.name[[i]]), format = "GTiff", overwrite = TRUE)
+  writeRaster(DOB_rounded, paste0(SpatialFilesPath, "./Inputs/DOB/DOBrounded/", "DOBrounded_", file.name[[i]]), format = "GTiff", overwrite = TRUE)
+  #writeRaster(DOB_rounded, paste0("./Inputs/Rasters/DOB/", "DOBrounded_", file.name[[i]]), format = "GTiff", overwrite = TRUE)
 }
-
-
