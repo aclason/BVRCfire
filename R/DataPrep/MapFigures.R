5584a95c0784eadb3c7206f77ecb9a15c98e5754
Ingrid Farnell <farnellingrid@gmail.com>
2023-02-07 13:06:25 -0800

    Weather stations added to Fig 1

--- R/DataPrep/MapFigures.R
+++ R/DataPrep/MapFigures.R
@@ -7,7 +7,6 @@
 
 # Remove workspace
 rm(list=ls())
-gc()
 
 #------------------------------ Load libraries---------------------------------#
 ls <- c("tidyverse") # Data Management and Manipulation
@@ -46,7 +45,7 @@ all.cities.metadata <- bcdc_get_record("b678c432-c5c1-4341-88db-0d6befa0c7f8") #
 all.cities <- bcdc_get_data(all.cities.metadata, resource = "b678c432-c5c1-4341-88db-0d6befa0c7f8") # get the layer
 # filter cities for small scale map
 cities <- all.cities %>%
-  filter(NAME=="Smithers"| NAME=="Telkwa"| NAME=="Houstson" | NAME=="Burns Lake"| NAME=="Fraser Lake" | NAME=="Vanderhoof" | NAME=="Prince George" | NAME=="Fort St. James")
+  filter(NAME=="Smithers"| NAME=="Telkwa"| NAME=="Houston" | NAME=="Burns Lake"| NAME=="Fraser Lake" | NAME=="Vanderhoof" | NAME=="Prince George" | NAME=="Fort St. James")
 # filter cities for large scale map
 major.cities <- all.cities %>%
   filter(NAME=="Burns Lake"| NAME=="Prince George" | NAME=="Vancouver")
@@ -58,6 +57,16 @@ all.roads <- bcdc_get_data(all.roads.metadata, resource = "e37cc64b-41a6-491f-be
 # filter to just highway 16, 35, 27 
 highways <- all.roads %>%
   filter(HIGHWAY_NUMBER=="H16"| HIGHWAY_NUMBER=="H35"| HIGHWAY_NUMBER=="H27")
+
+# WEATHER STATIONS
+stations <- read_sf(paste0(SpatialFilesPath, "./Inputs/FireWeatherStations/crmp_network_geoserverPoint.shp"))
+stationID <- c("NADINA", "ZZ PARROTT", "HOUSTON(DUNGATE)", "PEDEN", "GRASSY PLAINS", 
+               "HOLY CROSS 2", "EAST OOTSA", "AUGIER LAKE", "Vanderhoof", "KLUSKUS", 
+               "MOOSE LAKE", "FORT ST JAMES", "NORTH CHILCO")
+stations <- stations %>%
+  dplyr::filter(., station_na %in% stationID)
+stations <- stations %>%
+  dplyr::filter(!network_na == "EC" & !network_na == "ARDA") # remove the stations that are not wildfire (except Vanderhoof station)
 
 
 #-------------------------------------------------------------#
@@ -75,7 +84,9 @@ fires <- ggplot() +
   geom_sf_text(data = cities, 
                aes(label = NAME), 
                size = 2.5, 
-               nudge_y = -0.1) +
+               vjust = -0.5,
+               hjust = -0.05) +
+  geom_sf(data = stations, colour = "brown4", aes(shape = "Weather stations")) +
   north(StudyFirePerims, symbol = 10, 
         location = "bottomleft") +
   scalebar(StudyFirePerims, dist = 50, dist_unit = "km", st.size = 2, 
@@ -84,17 +95,20 @@ fires <- ggplot() +
            border.size = 0.1) +
   geom_rect(aes( # add a rectangle around the fire area
     xmin = 910000,
-    ymin = 915000, 
-    xmax = 1150000, 
+    ymin = 885000, 
+    xmax = 1167000, 
     ymax = 1110000),
     fill = NA, 
     colour = "black", 
     size= 0.6
   ) +
-  coord_sf(xlim = c(910000, 1150000), ylim = c(915000, 1110000), expand = FALSE) + # got from bb doing head(x)
+  coord_sf(xlim = c(910000, 1167000), ylim = c(885000, 1110000), expand = FALSE) + # got from bb doing head(x)
   theme_void() +
-  theme(legend.position = "bottom") +
-  labs(fill = "2018 Wildfires")
+  labs(fill = "2018 Wildfires",
+       shape = NULL) +
+  theme(legend.position = "bottom",
+        legend.box = "vertical")
+
    
 fires
 
@@ -107,12 +121,13 @@ bc <- ggplot() +
   geom_sf_text(data = major.cities, 
           aes(label = NAME), 
           size = 2.5, 
-          nudge_y = -0.1) +
+          vjust = -0.5,
+          hjust = -0.05) +
   geom_sf(data = StudyFirePerims, fill = "black", colour = NA) +
   geom_rect(aes( # add a rectangle around the fire area
     xmin = 910000,
-    ymin = 915000, 
-    xmax = 1150000, 
+    ymin = 885000, 
+    xmax = 1167000, 
     ymax = 1110000),
     fill = NA, 
     colour = "black", 
@@ -125,17 +140,21 @@ bc
 final_map <- 
   ggdraw() +
   draw_plot(fires, 
-            scale = 1.5) +
+            scale = 1.45) +
   draw_plot(bc,
             x = -0.1, 
-            y = 0.61, 
+            y = 0.95, 
             width = 0.6, 
             height = 0.7) +
-  theme(aspect.ratio = 0.45)
+  theme(aspect.ratio = 0.4)
 
 final_map
 
-ggsave(filename = "StudyArea.svg", device = "svg", plot = final_map, path = "./BVRCfire/Outputs/Figures/", bg = "white")
+ggsave(filename = "StudyArea.svg", device = "svg", 
+       plot = final_map, path = "./Outputs/Figures/", 
+       bg = "white",
+       height = 7.4,
+       width = 7.4)
 
 
 
